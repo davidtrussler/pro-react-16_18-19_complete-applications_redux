2f115f51bc7e1c79e6017a735151dcec86a6197a
David Trussler <mail@davidtrussler.net>
2020-07-24 19:15:33 +0100

    Adds state data to Store

--- src/store/initialData.js
+++ src/store/initialData.js
@@ -1,37 +1,44 @@
 import {PRODUCTS, SUPPLIERS} from './dataTypes'; 
 
 export const initialData = {
-	[PRODUCTS]: [
-		{
-			id: 1, 
-			name: 'Kayak', 
-			category: 'Watersports', 
-			price: '275'
-		}, 
-		{
-			id: 2, 
-			name: 'Lifejacket', 
-			category: 'Watersports', 
-			price: '48.95'
-		}, 
-		{
-			id: 3, 
-			name: 'Football', 
-			category: 'Football', 
-			price: '19.50'
-		}
-	], [SUPPLIERS]: [
-		{
-			id: 1, 
-			name: 'Surf Dudes', 
-			city: 'Bournemouth', 
-			products: [1, 2]
-		}, 
-		{
-			id: 2, 
-			name: 'Field Supplies', 
-			city: 'London', 
-			products: [3]
-		}
-	]
+	modelData: {
+		[PRODUCTS]: [
+			{
+				id: 1, 
+				name: 'Kayak', 
+				category: 'Watersports', 
+				price: '275'
+			}, 
+			{
+				id: 2, 
+				name: 'Lifejacket', 
+				category: 'Watersports', 
+				price: '48.95'
+			}, 
+			{
+				id: 3, 
+				name: 'Football', 
+				category: 'Football', 
+				price: '19.50'
+			}
+		], [SUPPLIERS]: [
+			{
+				id: 1, 
+				name: 'Surf Dudes', 
+				city: 'Bournemouth', 
+				products: [1, 2]
+			}, 
+			{
+				id: 2, 
+				name: 'Field Supplies', 
+				city: 'London', 
+				products: [3]
+			}
+		]
+	}, 
+	stateData: {
+		editing: false, 
+		selectedId: -1, 
+		selectedType: PRODUCTS
+	}
 }
