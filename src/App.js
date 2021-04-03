09a8829f5d58d955bb00eeef89e1edb3b30a9ad9
David Trussler <mail@davidtrussler.net>
2021-04-03 22:35:11 +0100

    Adds StoreAccess component to display contents of data store

--- src/App.js
+++ src/App.js
@@ -5,21 +5,32 @@ import dataStore from './store';
 import {Selector} from './Selector'; 
 import {ProductDisplay} from './ProductDisplay'; 
 import {SupplierDisplay} from './SupplierDisplay'; 
+import {StoreAccess} from './store/storeAccess'; 
 
 export default class App extends Component {
   render() {
     return (
-    	<Provider store={dataStore}>
-				<Selector>
-					<ProductDisplay
-						name="Products"
-					/>
+    	<div className="container-fluid">
+    		<div className="row">
+    			<div className="col-3">
+    				<StoreAccess store={dataStore} />
+    			</div>
 
-					<SupplierDisplay
-						name="Suppliers"
-					/>
-				</Selector>
-    	</Provider>
+    			<div className="col">
+			    	<Provider store={dataStore}>
+							<Selector>
+								<ProductDisplay
+									name="Products"
+								/>
+
+								<SupplierDisplay
+									name="Suppliers"
+								/>
+							</Selector>
+			    	</Provider>
+    			</div>
+    		</div>
+    	</div>
   	) 
   }
 }
